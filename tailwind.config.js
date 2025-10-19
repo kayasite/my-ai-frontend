201d2f4346cca1ab70463093d53ba65b587d095a
kayasite <slgl_1014@yahoo.co.jp>
2025-10-20 02:42:15 +0900

    🐛 Render互換修正: Tailwind v3へダウングレード + postcss修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}"
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
@@ -21,4 +21,5 @@ export default {
   },
   plugins: [],
 };
+
 
